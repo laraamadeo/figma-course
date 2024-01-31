12332724242c0e208220d967b046f00a8c66c982
Lara Amadeo <laraamadeo@192.168.1.144>
2024-01-31 09:03:07 +0100

    change credits

--- src/app/modules/Credits.jsx
+++ src/app/modules/Credits.jsx
@@ -1,7 +1,7 @@
 export default () => {
     return <>
         <div className="flex flex-col items-center">
-            <p className="font-pixel sm:text-2xl text-xl">Web build with React + Vite + Tailwind</p>
+            <p className="font-pixel sm:text-2xl text-xl">Web build with NextJS + Tailwind</p>
             <p className="font-pixel sm:text-xl text-md">y sobre todo mucho</p>
             <p className="text-xl">💖</p>
         </div>
