3146811266f7ab8ef7a0a2d7babeb6c47b8e2731
Lara Amadeo <laraamadeo@192.168.1.144>
2024-01-31 09:23:14 +0100

    add gtag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Viga, VT323 } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const viga = Viga({
   subsets: ['latin'],
@@ -24,6 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${vt323.variable} ${viga.variable} overflow-x-hidden`}>{children}</body>
+      <GoogleAnalytics gaId="G-L5HF1P01ES" />
     </html>
   );
 }
